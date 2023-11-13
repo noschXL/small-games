dec6101c75aa89e5128590777cac7908c005eb72
noschXL <noschxl@icloud.com>
2023-11-13 12:51:34 +0100

    some things ig

--- sim.py
+++ sim.py
@@ -6,8 +6,8 @@ RES = (900,900)
 AQUA = (0,255,255)
 wn = pg.display.set_mode(RES)
 GRAVITY = 0.45
-DRAFT = 50000
-COLLISION_DAMPING = 0.9
+DRAFT = 0.85
+COLLISION_DAMPING = 1
 SIZE = 10
 RADIUS = 100
 DOT_SPACE = 50
@@ -27,7 +27,8 @@ class fluid:
 
     def update(self):
         self.velocity.y += GRAVITY
-        self.velocity / DRAFT
+        self.evening_out()
+        self.velocity * DRAFT
         if self.velocity.x >= MAX_VEL.x:
             self.velocity.x = MAX_VEL.x
         if self.velocity.y >= MAX_VEL.y:
@@ -37,6 +38,15 @@ class fluid:
         pg.draw.circle(wn,AQUA,self.position,SIZE)
 
     def collision(self):
+        for dot in fluid.dots:
+            if  math.sqrt((abs(self.position.x - dot.position.x)) ** 2 + (abs(self.position.y - dot.position.y)) ** 2) <= RADIUS * 2:
+                v1 = self.position
+                v2 = dot.position
+                nv = v2 - v1
+                m1 = pg.math.Vector2(mx1, my1).reflect(nv)
+                m2 = pg.math.Vector2(mx2, my2).reflect(nv)
+                mx1, my1 = m1.x, m1.y
+                mx2, my2 = m2.x, m2.y
         if self.position.y <= 0 + SIZE :
             self.velocity.y -= GRAVITY + 0.1
             self.velocity.y = abs(self.velocity.y)
@@ -58,11 +68,15 @@ class fluid:
         new_vel_main = pg.Vector2()
         for dot in fluid.dots:
             dist = math.sqrt((abs(self.position.x - dot.position.x)) ** 2 + (abs(self.position.y - dot.position.y)) ** 2) #satz des pythagoras
-            if dist >= 500:
+            if dist >= 500 or dist == 0:
                 continue
-            new_vel.x +=  1 /-(self.position.x - dot.position.x)
-            new_vel.y += 1 /-(self.position.y - dot.position.y)
+            if -(self.position.x - dot.position.x) != 0:
+                new_vel.x +=  1 /-(self.position.x - dot.position.x)
+            if -(self.position.y - dot.position.y) != 0:
+                new_vel.y += 1 /-(self.position.y - dot.position.y)
             vel_list.append(new_vel)
+            new_vel_main = self.average_vectors(vel_list)
+            self.velocity += (new_vel_main.x / 2, new_vel_main.y / 2)
 
     def average_vectors(self, Vector_list = []):
         x = 0
@@ -77,27 +91,6 @@ class fluid:
         ret_vec.y = y / len(Vector_list)
         return ret_vec
 
-    def smoothing_kernel(self,dst):
-        volume = math.pi * pow(RADIUS, 8) / 4
-        value = max(0, RADIUS * RADIUS - dst * dst)
-        return value * value / volume
-
-    def calc_density(self):
-        density = 0
-        for dot in fluid.dots:
-            position = dot.position
-            dst = pg.math.Vector2.magnitude(position - self.position)
-            influence = self.smoothing_kernel(dst)
-            density += MASS + influence
-        return density
-    def calc_property(self):
-        prop = 0
-        for dot in fluid.dots:
-            position = dot.position
-            dst = pg.math.Vector2.magnitude(position - self.position)
-            influence = self.smoothing_kernel(dst)
-            prop += 1
-
 dots_per_row = math.sqrt(DOTS)
 dots_per_col = (DOTS - 1) / dots_per_row + 1
 spacing = SIZE * 2 + DOT_SPACE
@@ -106,6 +99,8 @@ for i in range(DOTS):
     x = 450 + ((i % dots_per_row - dots_per_row / 2 + 0.5) * DOT_SPACE)
     y = 450 + ((i / dots_per_row - dots_per_col / 2 + 0.5) * DOT_SPACE)
     fluid.dots.append(fluid(x,y))
+    for dot in fluid.dots:
+        dot.velocity.x = 10
 
 def event_check():
     for event in pg.event.get():
@@ -116,7 +111,6 @@ def event_check():
 def update():
     wn.fill((0,0,0))
     for dot in fluid.dots:
-        dot.evening_out()
         dot.update()
     pg.display.flip()
 
