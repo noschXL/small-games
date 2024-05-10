9d4b1f7e9c20c4319d2bb4b21518ca1cdaf4edf4
noschXL <noschxl@icloud.com>
2024-05-10 10:29:26 +0200

    made clouds using perlin noise

--- other_stuff/test.py
+++ other_stuff/test.py
@@ -1,21 +1,11 @@
 import pygame
 from threading import Thread
-import queue
 from perlin_noise import PerlinNoise
 from sys import exit
 
 width = 600
 height = 600
 
-class ReturnableThread(Thread):
-    # This class is a subclass of Thread that allows the thread to return a value.
-    def __init__(self,group = None, target = None, name = None, daemon = None,*args, **kwargs):
-        Thread.__init__(self, group, target, name, args, kwargs, deamon) # type: ignore
-        self.target = target
-        self.result = None
-    
-    def run(self) -> None:
-        self.result = self.target()
 
 pygame.init()
 screen = pygame.display.set_mode((width,height))
@@ -26,45 +16,48 @@ noise2 = PerlinNoise(octaves=6)
 noise3 = PerlinNoise(octaves=12)
 noise4 = PerlinNoise(octaves=24)
 
-def gen_row(i, xpix,ypix):
+def gen_row(i, xpix,ypix, offset):
+    surf = pygame.Surface((1,1))
+    print(offset)
     for j in range(ypix):
-        noise_val = noise1([i/xpix, j/ypix])
-        noise_val += 0.5 * noise2([i/xpix, j/ypix])
-        noise_val += 0.25 * noise3([i/xpix, j/ypix])
-        noise_val += 0.125 * noise4([i/xpix, j/ypix])
-        noise_val = abs(noise_val)
-        noise_val += 0.35
-
-    return [(round(noise_val) * 255,round(noise_val) * 255,round(noise_val) * 255), ((i * 1,j * 1),(1,1))]
+        noise_val = noise1([(i + offset)/xpix, j/ypix])
+        noise_val += 0.5 * noise2([(i + offset)/xpix, j/ypix])
+        noise_val += 0.25 * noise3([(i + offset)/xpix, j/ypix])
+        noise_val += 0.125 * noise4([(i + offset)/xpix, j/ypix])
+        noise_val = max(noise_val,0)
+        noise_val += 0.5
+        noise_val *= 255
+        surf.fill((255,255,255))
+        surf.set_alpha(noise_val)
+        screen.blit(surf, ((i+offset,j),(1,1)))
 
 def gen_map(xpix, ypix):
     threads = []
-    retmap = []
-    for i in range(xpix):
-        thread = ReturnableThread(target=gen_row, args=[i, xpix, ypix])
+    for i in range(int(xpix / 2)):
+        thread = Thread(target=gen_row, args=[i, xpix, ypix])
         threads.append(thread)
         thread.start()
 
     i = 0
     for thread in threads:
         thread.join()
-        retmap.append(thread.result)
         print(f"thread {i} is finished")
         i += 1
-    return retmap
+#curmap = gen_map(600,600)
 
-curmap = gen_map(600,600)
-
+i = 0
+offset = 0
+screen.fill((00,255,255))
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
-
-    for colum, row in enumerate(curmap):
-        pygame.draw.rect(screen, colum[0], colum[1])
-
-
+    gen_row(i,600,600,offset)
+    i += 1
+    if i >= 600:
+        offset += 1
+        screen.blit(screen,(-1,0))
     pygame.display.flip()
     clock.tick(60)
